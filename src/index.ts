ef2e2fc66df732655b551687eaf61e1db7708ecb
replyrebuff <44467554+replyrebuff@users.noreply.github.com>
2023-01-19 17:36:31 +0100

    added personastate

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,15 @@ import { config } from "dotenv";
 config();
 
 var previousGame = null;
+var previousPersonaState = null;
 
-async function getSteamGame() {
+async function getPlayerSummaries() {
 	var response = await fetch(
 		`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${process.env.STEAM_API_KEY}&steamids=${process.env.TARGET_STEAM_ID}`
 	);
 	var data = await response.json();
-	var game = data.response.players[0].gameextrainfo;
-	return game;
+
+	return data.response.players[0];
 }
 
 async function sendMessageToDiscord(message: string) {
@@ -32,14 +33,36 @@ async function sendMessageToDiscord(message: string) {
 }
 
 async function run() {
-	var game = await getSteamGame();
-	if (game != null && game != previousGame) {
-		sendMessageToDiscord(`<@${process.env.TARGET_DISCORD_ID}> is now playing ${game}.`);
-	} else if (game == null && previousGame != null) {
-		sendMessageToDiscord(`<@${process.env.TARGET_DISCORD_ID}> stopped playing ${previousGame}.`);
+	var playerSummaries = await getPlayerSummaries();
+
+	var game = playerSummaries.gameextrainfo;
+	var personaState = playerSummaries.personastate;
+
+	if (game != previousGame) {
+		if (game != null) {
+			sendMessageToDiscord(`<@${process.env.TARGET_DISCORD_ID}> is now playing ${game}.`);
+		} else {
+			sendMessageToDiscord(`<@${process.env.TARGET_DISCORD_ID}> stopped playing ${previousGame}.`);
+		}
+	}
+
+	if (personaState != previousPersonaState) {
+		switch (personaState) {
+			case 0:
+				sendMessageToDiscord(`<@${process.env.TARGET_DISCORD_ID}> is offline.`);
+				break;
+			case 1:
+				sendMessageToDiscord(`<@${process.env.TARGET_DISCORD_ID}> is online.`);
+				break;
+			case 3:
+				sendMessageToDiscord(`<@${process.env.TARGET_DISCORD_ID}> is away.`);
+			default:
+				break;
+		}
 	}
 
 	previousGame = game;
+	previousPersonaState = personaState;
 }
 
 setInterval(run, 1000);
